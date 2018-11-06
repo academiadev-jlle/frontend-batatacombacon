d6d72955fecccb036a80d1a59de3373b43e1cd26
Jim Chien <43502351+JimChie@users.noreply.github.com>
2018-11-06 14:03:57 -0200

    Add files via upload
    Terminando pagina home. 
    Sem rotas ainda.
    Adição de navbar, carousel, footer, localização e forms do home page. Igual no misc/index.html

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarouselComponent } from './carousel/carousel.component';
+import { HomeContentComponent } from './home-content/home-content.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    CarouselComponent,
+    HomeContentComponent
+  ],
+  exports: [
+    CarouselComponent,
+    HomeContentComponent
+  ]
 })
 export class PublicModule { }
