66d62091fef70fda10e9daadf31ec11310d19a52
Jim Chien <jim_chien_br@hotmail.com>
2018-11-08 07:45:40 -0200

    Adicionando Rotas básicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppComponent } from './app.component';
 import { PrivateModule } from './private/private.module';
 import { PublicModule } from './public/public.module';
 import { SharedModule } from './shared/shared.module';
+import { routing } from './app.routing';
+import { HomeComponent } from './home/home.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+import { ContatoComponent } from './contato/contato.component';
+import { DiscoverComponent } from './discover/discover.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CadastroComponent,
+    LoginComponent,
+    ContatoComponent,
+    DiscoverComponent
   ],
   imports: [
     BrowserModule,
     SharedModule,
     PrivateModule,
-    PublicModule
+    PublicModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
