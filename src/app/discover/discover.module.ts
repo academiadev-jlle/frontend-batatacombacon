642783785c75c53bb5dff212de9403f2244e4d40
Jim Chien <jim_chien_br@hotmail.com>
2018-11-09 00:07:30 -0200

    Add page Discover Pets

--- src/app/discover/discover.module.ts
+++ src/app/discover/discover.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { DiscoverRoutingModule } from './discover-routing.module';
 import { DiscoverComponent } from './discover.component';
+import { PetsDetailComponent } from './pets-detail/pets-detail.component';
+import { PetsListItemComponent } from './pets-list-item/pets-list-item.component';
+import { FilterListItemComponent } from './filter-list-item/filter-list-item.component';
 
 @NgModule({
   imports: [
@@ -11,7 +14,10 @@ import { DiscoverComponent } from './discover.component';
   ],
   exports: [],
   declarations: [
-    DiscoverComponent
+    DiscoverComponent,
+    PetsDetailComponent,
+    PetsListItemComponent,
+    FilterListItemComponent
   ]
 })
 export class DiscoverModule { }
---
+++ src/app/discover/pets-list-item/pets-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pets-list-item',
+  templateUrl: './pets-list-item.component.html',
+  styleUrls: ['./pets-list-item.component.scss']
+})
+export class PetsListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
