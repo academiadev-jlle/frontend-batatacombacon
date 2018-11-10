47824776cc181609b160092c999d0d7aee08437d
Jim Chien <jim_chien_br@hotmail.com>
2018-11-09 23:26:12 -0200

    Refatorando feature/discover

--- src/app/discover/pets-list-item/pets-list-item.component.ts
+++ src/app/discover/pets-list-item/pets-list-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { PetOptions } from './pets-list-item.options';
 
 @Component({
   selector: 'app-pets-list-item',
@@ -7,7 +9,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PetsListItemComponent implements OnInit {
 
-  constructor() { }
+  pets: PetOptions[] = [];
+
+  constructor() {
+    this.pets.push(...[{
+      id: 1,
+      name: 'Pé de pano',
+      photo: 'https://www.paulickreport.com/wp-content/uploads/2015/08/Happy_horse.jpg',
+      description: 'Oi! Fui abandonado e preciso de um novo lar. Sou bem divertido :D',
+      isAdoption: false,
+      isLost: true
+    }, {
+      id: 2,
+      name: 'Cãopeão',
+      photo: 'http://talkingship.com/wp/wp-content/uploads/2013/12/dog-laughing-from-Laugh-It-Out-with-Luisa-Thwaites-443x325.jpg',
+      description: 'Yeahhhhhh!',
+      isAdoption: true,
+      isLost: false
+    }, {
+      id: 3,
+      name: 'Baby',
+      photo: 'https://www.bestfunnies.com/wp-content/uploads/2012/06/Funny-Pig-48.jpg',
+      description: 'Perdi o caminho de casa. Conhece meu dono? Quer me adotar?',
+      isAdoption: false,
+      isLost: false
+    }, {
+      id: 4,
+      name: 'Balanoalvo',
+      photo: 'https://www.paulickreport.com/wp-content/uploads/2015/08/Happy_horse.jpg',
+      description: 'Oi! Fui abandonado e preciso de um novo lar. Sou bem divertido :D',
+      isAdoption: false,
+      isLost: true
+    }, {
+      id: 5,
+      name: 'Cachurros',
+      photo: 'http://talkingship.com/wp/wp-content/uploads/2013/12/dog-laughing-from-Laugh-It-Out-with-Luisa-Thwaites-443x325.jpg',
+      description: 'Yeahhhhhh!',
+      isAdoption: true,
+      isLost: false
+    }, {
+      id: 6,
+      name: 'Bacon',
+      photo: 'https://www.bestfunnies.com/wp-content/uploads/2012/06/Funny-Pig-48.jpg',
+      description: 'Perdi o caminho de casa. Conhece meu dono? Quer me adotar?',
+      isAdoption: false,
+      isLost: false
+    }, {
+      id: 7,
+      name: 'Coice',
+      photo: 'https://www.paulickreport.com/wp-content/uploads/2015/08/Happy_horse.jpg',
+      description: 'Oi! Fui abandonado e preciso de um novo lar. Sou bem divertido :D',
+      isAdoption: false,
+      isLost: true
+    }, {
+      id: 8,
+      name: 'Doggo',
+      photo: 'http://talkingship.com/wp/wp-content/uploads/2013/12/dog-laughing-from-Laugh-It-Out-with-Luisa-Thwaites-443x325.jpg',
+      description: 'Yeahhhhhh!',
+      isAdoption: true,
+      isLost: false
+    }, {
+      id: 9,
+      name: 'Batata',
+      photo: 'https://www.bestfunnies.com/wp-content/uploads/2012/06/Funny-Pig-48.jpg',
+      description: 'Perdi o caminho de casa. Conhece meu dono? Quer me adotar?',
+      isAdoption: false,
+      isLost: false
+    }]);
+  }
 
   ngOnInit() {
   }
