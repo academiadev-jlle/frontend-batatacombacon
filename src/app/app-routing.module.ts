207cc8c6527060892efde9eedae98e76bfae9221
Jim Chien <jim_chien_br@hotmail.com>
2018-11-16 22:47:38 -0200

    Add page-not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { HomeComponent } from './pages/public/home/home.component';
 import { CadastroComponent } from './pages/public/cadastro/cadastro.component';
 import { LoginComponent } from './pages/private/login/login.component';
 import { ContatoComponent } from './pages/public/contato/contato.component';
+import { PageNotFoundComponent } from './pages/public/page-not-found/page-not-found.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/discover', pathMatch: 'full' },
     { path: 'discover', loadChildren: './pages/public/discover/discover.module#DiscoverModule'},
     { path: 'cadastro', component: CadastroComponent},
     { path: 'login', component: LoginComponent},
-    { path: 'contato', component: ContatoComponent}
+    { path: 'contato', component: ContatoComponent},
+    {path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
