578590ef557ec93325e80d6a89181110636b8e09
Marco Antonio Yamada <marcoayamada@gmail.com>
2018-11-20 15:35:17 -0200

    changed server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const dist = 'dist/frontend-batatacombacon';
 
+app.use(allowCrossDomain);
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.join(__dirname, dist)));
 
