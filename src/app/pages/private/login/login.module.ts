9c80483bdd9723816112e37df18d59cbfc74c4f5
Marco <marcoayamada@gmail.com>
2018-11-20 12:15:11 -0200

    add profile in module

--- src/app/pages/private/login/login.module.ts
+++ src/app/pages/private/login/login.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { ProfileComponent } from '../profile/profile.component';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import { LoginComponent } from './login.component';
   ],
   exports: [],
   declarations: [
-    LoginComponent
+    LoginComponent,
+    ProfileComponent
   ]
 })
 export class LoginModule { }
