9986f0095c2b712e023e7f4652e5ceec52a20b38
JimChie <jim_chien_br@hotmail.com>
2018-11-21 10:38:44 -0200

    Finalizando conflitos

--- src/app/shared/private/pet-forms/pet-forms.component.ts
+++ src/app/shared/private/pet-forms/pet-forms.component.ts
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-=======
 import { Component} from '@angular/core';
->>>>>>> 91571b1261c5dc275043ec49704c6526923f304f
 
 @Component({
   selector: 'app-pet-forms',
@@ -12,19 +7,7 @@ import { Component} from '@angular/core';
 })
 export class PetFormsComponent {
 
-  constructor(private fb: FormBuilder) { }
-
-  petForm = this.fb.group({
-    nome: [''],
-    especie: [''],
-    Porte: [''],
-    Categoria: [''],
-    Sexo: [''],
-    Estado: [''],
-    Data: [''],
-    Localização: [''],
-    Descrição: [''],
-  })
+  constructor() { }
 
 
 }
