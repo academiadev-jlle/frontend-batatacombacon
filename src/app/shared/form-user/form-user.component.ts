8b05470e34d6fdd6d10ee1e4ecb89212ca621c63
Marco Antonio Yamada <marcoayamada@gmail.com>
2018-11-22 19:03:51 -0200

    cadastro - informaçoes do usuario - ok

--- src/app/shared/form-user/form-user.component.ts
+++ src/app/shared/form-user/form-user.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ConfirmPasswordValidator } from './confirm-password.validator';
+import { Usuario } from 'src/app/classes/usuario/usuario';
 
 @Component({
   selector: 'app-form-user',
   templateUrl: './form-user.component.html',
   styleUrls: ['./form-user.component.scss']
 })
-export class FormUserComponent implements OnInit {
+export class FormUserComponent implements OnInit, OnChanges {
   
+  @Input() usuarioProfile: Usuario;
   @Output() messageEvent = new EventEmitter<FormGroup>();
+  
   usuario: FormGroup;
   submitted = false;
   
@@ -20,6 +23,7 @@ export class FormUserComponent implements OnInit {
     get f() { return this.usuario.controls; }
     
     ngOnInit() {
+
       this.usuario = this.formBuilder.group({
         nome: ['', Validators.required],
         sobrenome: [''],
@@ -30,7 +34,25 @@ export class FormUserComponent implements OnInit {
         validator: ConfirmPasswordValidator.MatchPassword
       });
     }
-    
+
+    ngOnChanges(changes: SimpleChanges) {
+
+      // verificando se usuarioProfile esta na lista de changese e se ele nao for nulo
+      // se ele for nulo quer dizer que é um cadastro novo. Não tem que preencher.
+      if("usuarioProfile" in changes && this.usuarioProfile!==undefined){
+        this.fillUsuario();
+      }
+    }
+
+    fillUsuario(){
+      this.usuario.patchValue({
+        nome: this.usuarioProfile.nome,
+        email: this.usuarioProfile.email,
+        senha: "",
+        confirmSenha: ""
+      });
+    }
+   
     sendMessage() {
       this.messageEvent.emit(this.usuario);
     }
