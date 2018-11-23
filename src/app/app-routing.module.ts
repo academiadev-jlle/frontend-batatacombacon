d330a46885ee4a14cab74d06757a4b63f9311196
Jim Chien <jim_chien_br@hotmail.com>
2018-11-23 19:29:40 -0200

    Alteraçoes Conflitos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/public/home/home.component';
 import { CadastroComponent } from './pages/public/cadastro/cadastro.component';
+import { LoginComponent } from './pages/public/login/login.component';
 import { ContatoComponent } from './pages/public/contato/contato.component';
 import { PageNotFoundComponent } from './pages/public/page-not-found/page-not-found.component';
+
+import { ProfileComponent } from './pages/private/profile/profile.component';
 
 
 const routes: Routes = [
     { path: '', redirectTo: '/discover', pathMatch: 'full' },
     { path: 'discover', loadChildren: './pages/public/discover/discover.module#DiscoverModule'},
     { path: 'cadastro', component: CadastroComponent},
-    { path: 'login', loadChildren: './pages/private/login/login.module#LoginModule'},
+    { path: 'login', component: LoginComponent},
     { path: 'contato', component: ContatoComponent},
+
+    //Essa rota será alterada para validar login. Criada assim para desenvolvimento apenas.
+    { path: 'profile', component: ProfileComponent },
+
+    // essas 2 tem que ficar por ultimo. Rotas depois delas nao funcionam
     { path: '404', component: PageNotFoundComponent},
-    {path: '**', redirectTo: '/404', pathMatch: 'full' }
+    { path: '**', redirectTo: '/404'},
 ];
 
 @NgModule({
--- src/app/pages/private/login/login.module.ts
+++ src/app/pages/private/login/login.module.ts
@@ -1,25 +1,17 @@
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { ProfileComponent } from '../profile/profile.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule,
-    FormsModule,
-    ReactiveFormsModule,
-    LoginRoutingModule,
+    LoginRoutingModule
   ],
   exports: [],
   declarations: [
-    LoginComponent,
-    ProfileComponent
+    LoginComponent
   ]
 })
 export class LoginModule { }
