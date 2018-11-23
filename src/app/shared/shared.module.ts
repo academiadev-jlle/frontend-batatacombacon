7c47cc569148018045bde0e584b4103044dc93a3
Jim Chien <jim_chien_br@hotmail.com>
2018-11-23 19:07:13 -0200

    Finalizado edit pet

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,9 +6,6 @@ import { NavbarComponent } from './public/navbar/navbar.component';
 import { FooterComponent } from './public/footer/footer.component';
 import { NavbarLoggedComponent } from './private/navbar-logged/navbar-logged.component';
 import { PetStatusPipe } from './public/pipe/pet-status.pipe';
-import { PetFormsComponent } from './private/pet-forms/pet-forms.component';
-import { DragNDropImageComponent } from './private/drag-n-drop-image/drag-n-drop-image.component';
-
 
 @NgModule({
   imports: [
@@ -20,16 +17,12 @@ import { DragNDropImageComponent } from './private/drag-n-drop-image/drag-n-drop
     NavbarComponent,
     NavbarLoggedComponent,
     PetStatusPipe,
-    PetFormsComponent,
-    DragNDropImageComponent
   ],
   declarations: [
     FooterComponent,
     NavbarComponent,
     NavbarLoggedComponent,
     PetStatusPipe,
-    PetFormsComponent,
-    DragNDropImageComponent
   ]
 })
 export class SharedModule { }
