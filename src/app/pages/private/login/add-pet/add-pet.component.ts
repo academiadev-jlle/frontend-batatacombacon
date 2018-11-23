29955ba869585ec72901e91247adbf484ce554d2
JimChie <jim_chien_br@hotmail.com>
2018-11-23 14:23:39 -0200

    Habilitando show and hide do input

--- src/app/pages/private/login/add-pet/add-pet.component.ts
+++ src/app/pages/private/login/add-pet/add-pet.component.ts
@@ -11,8 +11,9 @@ import { PetService } from 'src/app/services/pet.service';
 export class AddPetComponent {
 
   imageSrc: String;
-  background = document.getElementsByClassName('preview-image-container');
-  img = document.getElementsByClassName('preview-image-url');
+  // background = document.getElementsByClassName('preview-image-container');
+  // img = document.getElementsByClassName('preview-image-url');
+  showBtn: boolean = true;
 
   constructor(private fb: FormBuilder, private petService: PetService, private renderer: Renderer2) { }
 
@@ -35,59 +36,63 @@ export class AddPetComponent {
     }),
   });
 
-  @ViewChild('imagePreview') imagePreview;
-
   readURL(event): void {
     if (event.target.files && event.target.files[0]) {
       const file = event.target.files[0];
-      const reader = new FileReader();
-      reader.onload =  (event) => {
-        console.log('readfiles event ===== ', event);
-        const fileReader = event.target as FileReader;
-        let image = new Image();
-        image.src = fileReader.result;
-        image.insertAdjacentHTML( 'afterbegin' , 'class="preview-image-url" ');
-        this.imagePreview.nativeElement.appendChild(image);
-      };
-      reader.readAsDataURL(file);
       // const reader = new FileReader();
-      // reader.onload = e => this.imageSrc = reader.result;
-
+      // reader.onload =  (event) => {
+      //   console.log('readfiles event ===== ', event);
+      //   const fileReader = event.target as FileReader;
+      //   let image = new Image();
+      //   image.src = fileReader.result;
+      //   image.insertAdjacentHTML( 'afterbegin' , 'class="preview-image-url" ');
+      //   this.imagePreview.nativeElement.appendChild(image);
+      // };
       // reader.readAsDataURL(file);
+      const reader = new FileReader();
+      reader.onload = e => this.imageSrc = reader.result;
+
+      this.hideBtn();
+      reader.readAsDataURL(file);
     }
   }
 
-  onDragOver(event: Event) {
-    event.preventDefault();
-    event.stopPropagation();
-    this.renderer.setStyle(this.background[0], 'background-color', '#999');
+  hideBtn() {
+    this.showBtn = !this.showBtn;
   }
 
-  onDragLeave(event: Event) {
-    event.preventDefault();
-    event.stopPropagation();
-    this.renderer.setStyle(this.background[0], 'background-color', '#FFF');
-  }
+// @ViewChild('imagePreview') imagePreview;
 
-  onDrop(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    this.renderer.setStyle(this.background[0], 'background-color', '#FFF');
-    let file = event.dataTransfer.files[0];
-    let reader = new FileReader();
-    reader.onload =  (event) => {
-      console.log('readfiles event ===== ', event);
-      const fileReader = event.target as FileReader;
-      let image = new Image();
-      image.src = fileReader.result;
-    //   this.imageSrc = reader.result;
-    //   document.getElementsByClassName("preview-image-url").src = image.src;
-      image.insertAdjacentHTML( 'afterbegin' , 'class="preview-image-url" ');
-    //   image.setAttribute('class', 'preview-image-url');
-      this.imagePreview.nativeElement.appendChild(image);
-    };
-    reader.readAsDataURL(file);
-  }
+  // onDragOver(event: Event) {
+  //   event.preventDefault();
+  //   event.stopPropagation();
+  //   this.renderer.setStyle(this.background[0], 'background-color', '#999');
+  // }
+
+  // onDragLeave(event: Event) {
+  //   event.preventDefault();
+  //   event.stopPropagation();
+  //   this.renderer.setStyle(this.background[0], 'background-color', '#FFF');
+  // }
+
+  // onDrop(event) {
+  //   event.preventDefault();
+  //   event.stopPropagation();
+  //   this.renderer.setStyle(this.background[0], 'background-color', '#FFF');
+  //   let file = event.dataTransfer.files[0];
+  //   let reader = new FileReader();
+  //   reader.onload =  (event) => {
+  //     console.log('readfiles event ===== ', event);
+  //     const fileReader = event.target as FileReader;
+  //     let image = new Image();
+  //     image.src = fileReader.result;
+  //     image.className = 'preview-image-url';
+  //     console.log(document.getElementsByClassName('preview-image-container'));
+  //     image.style = "display: block; margin: auto; width: 100%; height: 100%; max-width: 100%; max-height: 100%; position: absolute; top: 0; bottom: 0; left: 0; right: 0;";
+  //     this.imagePreview.nativeElement.appendChild(image);
+  //   };
+  //   reader.readAsDataURL(file);
+  // }
 
   ClickAddPet() {
     this.petService.addPet(this.petForm.value)
