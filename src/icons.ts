428a5e09cf813d5a4ca53ffabf050048d60163c6
Jim Chien <jim_chien_br@hotmail.com>
2018-11-27 17:33:08 -0200

    retirando google+

--- src/icons.ts
+++ src/icons.ts
@@ -1,14 +1,13 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
-import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons/faGooglePlusG';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons/faWhatsapp';
 import { faLink } from '@fortawesome/free-solid-svg-icons/faLink';
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
 
 
 const icons = [
-  faFacebookF, faTwitter, faGooglePlusG,
+  faFacebookF, faTwitter,
   faWhatsapp, faLink
 ];
 
