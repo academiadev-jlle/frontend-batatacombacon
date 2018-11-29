cc79fcbfeae6b7ce1e2e8e55d2bf1c1f1402db28
Jim Chien <jim_chien_br@hotmail.com>
2018-11-29 18:58:39 -0200

    Alterando form-pet component para enviar dados do form

--- src/app/shared/private/form-pet/form-pet.component.ts
+++ src/app/shared/private/form-pet/form-pet.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { Pet } from 'src/app/classes/pets/pet';
 
 @Component({
   selector: 'app-form-pet',
   templateUrl: './form-pet.component.html',
   styleUrls: ['./form-pet.component.scss']
 })
-export class FormPetComponent implements OnInit {
+export class FormPetComponent implements OnInit, OnChanges {
+
+  @Input() petProfile: Pet;
+  @Output() messageEvent = new EventEmitter<FormGroup>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnChanges() {
+
+  }
+
+  
 }
