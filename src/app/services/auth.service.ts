670b083d75f3d0a9fcb212970b907aff1ddd164f
Marco Antonio Yamada <marcoayamada@gmail.com>
2018-12-01 14:41:10 -0200

    alterado auth.service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -17,8 +17,10 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class AuthService {
-  private access_token = '';
-  private url = "https://reqres.in/api";
+  
+  //private access_token = '';
+  //private url = "https://reqres.in/api";
+
   private logged = new BehaviorSubject<boolean>(false);
 
   constructor(private http: HttpClient, private router: Router, private oauth: OAuthService) { }
@@ -35,25 +37,24 @@ export class AuthService {
     );
   }
 
+  // login(user: string, pass: string): Observable<any>{
 
-  login(user: string, pass: string): Observable<any>{
+  //   user = "peter@klaven";
+  //   pass = "cityslicka";
 
-    user = "peter@klaven";
-    pass = "cityslicka";
+  //   // aqui vai o body com a requisicao. OAuth2 pede 'grant_type: password' ?
+  //   const bodyPayload = {"email": user, "password": pass /*'grant_type':'password');*/};
 
-    // aqui vai o body com a requisicao. OAuth2 pede 'grant_type: password' ?
-    const bodyPayload = {"email": user, "password": pass /*'grant_type':'password');*/};
-
-    return this.http.post(`${this.url}/login`, bodyPayload, httpOptions)
-      .pipe(
-        map(ret => {
-          this.access_token = ret['token'];
-          this.logged.next(true);
-          return ret;
-        }),
-        catchError(this.handleError)
-      );
-  }
+  //   return this.http.post(`${this.url}/login`, bodyPayload, httpOptions)
+  //     .pipe(
+  //       map(ret => {
+  //         this.access_token = ret['token'];
+  //         this.logged.next(true);
+  //         return ret;
+  //       }),
+  //       catchError(this.handleError)
+  //     );
+  // }
 
   logout(){
     // TODO: mais alguma implementação?
